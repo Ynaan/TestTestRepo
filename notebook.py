bde03da95df27c8b91d8d3c0ea831b721037339a
Yanan Xie <67939624+Ynaan@users.noreply.github.com>
2021-08-29 21:20:27 -0700

    savee

--- notebook.py
+++ notebook.py
@@ -1,2 +1,60 @@
 # Databricks notebook source
-# MAGIC %sql select * from pavan
+This is the oringinal text offline offline offline offline back online back online back online
+
+# COMMAND ----------
+
+0
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %sh whoami
+
+# COMMAND ----------
+
+# MAGIC %sh ls -al /tmp/dbfs-command-result
+
+# COMMAND ----------
+
+# MAGIC %sh cat /tmp/dbfs-command-result/6312503306496875611-a8304566-fe17-4b8d-a72c-234bdf9863a6
+
+# COMMAND ----------
+
+# MAGIC %pip install ipykernel==5.1.2
+
+# COMMAND ----------
+
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+
+# COMMAND ----------
+
+# MAGIC %sh ls /tmp
+
+# COMMAND ----------
+
+# MAGIC %sh less  /tmp/driver-env.sh
+
+# COMMAND ----------
+
+displayHTML('x' * 2 * 1024 * 1024)
+
+# COMMAND ----------
+
+display(spark.range(1, 100))
+
+# COMMAND ----------
+
+# MAGIC %md ## good
+
+# COMMAND ----------
+
+2+2
